0b066d0677840e96b43200c91776ff29e7ce2ed4
Julia Burke <julia.burke00@gmail.com>
2014-04-05 09:52:17 -0600

    Post 4.2

---
+++ _site/README.md
@@ -0,0 +1,15 @@
+# Stellenbauchery
+
+This is the Jekyll source code for [Stellenbauchery](http://stellenbauchery.com),
+the website of Julia Burke.
+
+## Thanks
+
+This site wouldn't be possible without the awesome contributors to Jekyll,
+since I would rather shoot myself than work within the confines of a crappy
+CMS and really didn't feel like maintaining a Sinatra/Rails app for a simple
+blog.
+
+## License
+
+See LICENSE.
---
+++ _site/javascripts/email.js
@@ -0,0 +1,18 @@
+//evaluate this code inline to generate a proper email address
+//(via Hivelogic Enkoder http://hivelogic.com/enkoder/index.php)
+
+//<![CDATA[
+<!--
+var x="function f(x){var i,o=\"\",ol=x.length,l=ol;while(x.charCodeAt(l/13)!" +
+"=116){try{x+=x;l+=l;}catch(e){}}for(i=l-1;i>=0;i--){o+=x.charAt(i);}return " +
+"o.substr(0,ol);}f(\")51,\\\"620\\\\430\\\\020\\\\300\\\\310\\\\620\\\\Ft\\\\"+
+"130\\\\ILA100\\\\}kqyG;vpyzq5IgsJ0?>hgy771\\\\k&foiqi<#\\\\\\\\330\\\\620\\" +
+"\\610\\\\610\\\\8Y130\\\\630\\\\230\\\\000\\\\>S530\\\\500\\\\P300\\\\700\\" +
+"\\410\\\\100\\\\610\\\\J700\\\\t\\\\G200\\\\310\\\\100\\\\0@=]:2)5/z{400\\\\"+
+":96z?;0=(610\\\\}|.!;=%h$-/7+zPJQUZW330\\\\dn\\\\PPF[220\\\\P410\\\\r\\\\60" +
+"0\\\\C@O\\\\\\\\OVU610\\\\jr771\\\\ucw}t\\\"(f};o nruter};))++y(^)i(tAedoCr" +
+"ahc.x(edoCrahCmorf.gnirtS=+o;721=%y;++y)51<i(fi{)++i;l<i;0=i(rof;htgnel.x=l" +
+",\\\"\\\"=o,i rav{)y,x(f noitcnuf\")"                                        ;
+while(x=eval(x));
+//-->
+//]]>
